f6696dadc9f4371b40f046ba2a3e3a0af6e96c11
Satyam Bansal <162490036+satty0104@users.noreply.github.com>
2025-08-07 19:17:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase
 import { getDatabase, ref, push, set } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 // Firebase configuration
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY_HERE",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID",
-    databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com"
-};
+// const firebaseConfig = {
+//     apiKey: "YOUR_API_KEY_HERE",
+//     authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+//     projectId: "YOUR_PROJECT_ID",
+//     storageBucket: "YOUR_PROJECT_ID.appspot.com",
+//     messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+//     appId: "YOUR_APP_ID",
+//     measurementId: "YOUR_MEASUREMENT_ID",
+//     databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com"
+// };
 
 
 // Initialize Firebase
